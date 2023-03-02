c67f895386133d2f7f44816a320d76fa719aebeb
Gregory <gnazarov16@gmail.com>
2023-03-02 08:44:25 +0700

    build removed

--- build/routes/featuredRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-const collectionController_1 = require("../controllers/collectionController");
-const router = (0, express_1.Router)();
-router.get('/', (0, authController_1.protect)(), collectionController_1.getFeatured);
-exports.default = router;
-//# sourceMappingURL=featuredRoute.js.map
